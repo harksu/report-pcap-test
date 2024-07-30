054be9d4a56c0bc339ed3e1c45bcb9b56a4d6ecf
harksu <harksulim@naver.com>
2024-07-30 14:36:20 -0400

    feat: print source mac address

--- pcap-test.c
+++ pcap-test.c
@@ -1,6 +1,7 @@
 #include <pcap.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <libnet.h>
 
 void usage() {
 	printf("syntax: pcap-test <interface>\n");
@@ -24,6 +25,15 @@ bool parse(Param* param, int argc, char* argv[]) {
 	return true;
 }
 
+
+void print_mac_addr(struct libnet_ethernet_hdr* mac_hdr, const char* field){
+		printf("%s:",field);
+		for (int i = 0; i<ETHER_ADDR_LEN; i++){
+			i == ETHER_ADDR_LEN-1 ? printf("%02x\n",mac_hdr->ether_shost[i]) : printf("%02x:",mac_hdr->ether_shost[i]);
+		}
+}
+
+
 int main(int argc, char* argv[]) {
 	if (!parse(&param, argc, argv))
 		return -1;
@@ -37,6 +47,11 @@ int main(int argc, char* argv[]) {
 
 	while (true) {
 		struct pcap_pkthdr* header;
+
+		struct libnet_ethernet_hdr* eth_hdr; //ethernet header
+		struct libnet_ipv4_hdr* ipv4_hdr; // ipv4 header
+		struct libnet_tcp_hdr* tcp_hdr; // tcp header 
+
 		const u_char* packet;
 		int res = pcap_next_ex(pcap, &header, &packet);
 		if (res == 0) continue;
@@ -44,7 +59,11 @@ int main(int argc, char* argv[]) {
 			printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(pcap));
 			break;
 		}
-		printf("%u bytes captured\n", header->caplen);
+		if(tcp_hdr){
+			print_mac_addr(eth_hdr, "src_mac_addr");
+//		eth_hdr->ether_type && printf("ipv4_header => src_ip: %u dst_ip: %u \n", ipv4_hdr->ip_src, ipv4_hdr->ip_dst);
+//		printf("tcp_hedaer => src_port: %u dst_port: %u \n" , tcp_hdr->th_sport,tcp_hdr->th_dport);
+		}
 	}
 
 	pcap_close(pcap);
